690247d89d39c6d1c1ba3f2cf4bf82fdc702adb5
gobivadivu <gobikavadivu10@gmail.com>
2025-06-26 12:26:24 +0530

    Added user Registration functionality

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 LOGIN_URL = 'login'
 #redirect the user if not authenticated
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
